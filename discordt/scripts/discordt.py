d4f961283b0e495f18779dfd24b5183bdade7a25
jftaas <jftaas@harvard.wiki>
2015-09-28 21:43:33 +0800

    Add additional samples of interface usage.

--- discordt/scripts/discordt.py
+++ discordt/scripts/discordt.py
@@ -34,14 +34,25 @@ def main():
     # set to false if bot will be sending messages
     dtcontroller.local_message_display = True
 
-    # unfortunately, only on_message and
-    # on_ready have been implemented in dtclient
+    # if you need access to discord.py's Discord.Client
+    client = dtcontroller.dtclient.discordClient
+
+    # only on_message and
+    # on_ready have implementations in dtclient
     @dtcontroller.dtclient.event
     def on_message(message):
         pass
 
     @dtcontroller.dtclient.event
-    def on_ready(message):
+    def on_ready():
+        # printing message on output box
+        dtcontroller.print_output('Hello world!')
+
+    # if you need the rest of the events:
+    # but do not use on on_message and on_ready
+    # as they will just get overwritten
+    @dtcontroller.dtclient.discordClient.event
+    def on_disconnect():
         pass
 
     # dtcontroller.run() -- just starts interface, must use /login
